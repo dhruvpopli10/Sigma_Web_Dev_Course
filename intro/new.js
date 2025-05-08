cae1d9ac3143e4afee0a1719a422f8fd6cd5494c
dhruv popli <dhruvpopli11@gmail.com>
2025-05-08 10:53:15 +0530

    intial commit

---
+++ intro/new.js
@@ -0,0 +1,4 @@
+console.log("hello World")
+console.log("code is running")
+console.log("console is also running")
+console.log("code is looking like a wow")
---
+++ intro/script.js
@@ -0,0 +1,13 @@
+alert("Hello world")
+console.log("hi")
+var a=prompt("Enter ur number")
+var isTrue=confirm("do u want to exit this page and blast this computer")
+if(isTrue){
+    console.log("computer is blasting")
+}
+else{
+    console.log("computer is not blasting")
+}
+console.log("ur number is "+a)
+document.title="hey i am good"
+document.body.style.backgroundColor="red"
